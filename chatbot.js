6532c36a9ac3ab17a36148a7e33312d6667d177d
paul <123942274+paulvengesa@users.noreply.github.com>
2025-09-09 14:23:50 +0200

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,5 +1,5 @@
 /* --------- CONFIG --------- */
-const API_BASE = window.location.hostname === 'localhost' ? 'http://localhost:8000' : 'https://your-production-backend.com';
+const API_BASE = window.location.hostname === 'https://paulvengesa.github.io/chatbot';
 /* -------------------------- */
 
 const $messages = document.getElementById('messages');
